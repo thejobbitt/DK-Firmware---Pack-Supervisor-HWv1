e04a6ca926de522d5d87735a4fd95abdaf7c3800
thejobbitt <nick@thejobbitt.com>
2023-01-06 17:58:34 -0500

    Changed to structure

--- include/celltypes.h
+++ include/celltypes.h
@@ -3,6 +3,7 @@
 // Date       	Who		WAS/IS changes
 //---------------------------------------------------------------------------
 // 10/05/22		NHJ 	Initial creation
+// 01/06/22     NHJ     Formatting and varibale naming
 
 #include <Arduino.h>
 #include <NCP18.h>
@@ -10,62 +11,88 @@
 #ifndef CELLTYPES_H
 #define CELLTYPES_H
 
+extern struct celltypes cell;
+
 //-LITHIUM CELL SPECIFICATIONS-------------------------------------------------------------------
 #ifdef LG_MH1                                       // cell parameters for LG MH1 3200mah
-    #define Vcell_HVD_Spec                  4.21f   // high voltage disconnect, 4.25 max leave room for 0.1% acc.
-    #define Vcell_Nominal_Spec              3.67f
-    #define Vcell_Low_Spec                  2.91f   // end voltage cutoff 2.5V is spec
-    #define Vcell_LVD_Spec                  2.91f   // low voltage disconnect, all off
-    #define Vcell_Balance                   4.11f   // voltage above where Cells will balance
-    #define Vcell_Charge_Low_Cutoff         2.00f
-    #define Vcell_Charge_High_Cutoff        4.20f
-    #define Vcell_Charge_Trickle            2.80f   // voltage under where trickle charge should happen  
-    #define Vcell_Charge_Bulk               4.05f   // voltage under where the bulk of the charge should happen
-    #define Vcell_Charge_Off                4.15f   // voltage above where charger should turn off
-    #define Tcell_Charge_Low_Max_Cutoff     NTC_0C  // Temp where relay will turn off
-    #define Tcell_Charge_High_Max_Cutoff    NTC_45C // Temp where relay will turn off
-    #define Tcell_Charge_Low_Cutoff         NTC_3C  // Temp where charger will not attempt to charge
-    #define Tcell_Charge_High_Cutoff        NTC_43C // Temp where charger will not attempt to charge
-    #define Tcell_Charge_Taper_1            NTC_35C // Temp where charger will reduce current
-    #define Tcell_Charge_Taper_2            NTC_40C // Temp where charger will reduce current more
+    struct celltypes
+    {
+        // voltage specifications
+        const float volt_Nominal                    = 3.67f;   // nominal voltage
+        const float volt_Balance                    = 4.11f;   // balancing voltage
+        const float volt_HVD                        = 4.21f;   // high voltage disconnect
+        const float volt_LVD                        = 2.91f;   // low voltage disconnect
+        // charge voltages
+        const float volt_high_chargeCut             = 4.20f;   // charger upper cutoff voltage
+        const float volt_low_chargeCut              = 2.00f;   // charger lower cutoff voltage
+        const float volt_chargeTrickle              = 2.80f;   // voltage under where trickle charge should happen
+        const float volt_chargeBulk                 = 4.05f;   // voltage under where the bulk of the charge should happen
+        const float volt_chargeOff                  = 4.15f;   // voltage above where charger should turn off
+        // discharge temps
+        const u_int16_t temp_over_driveRelayCut     = NTC_60C;      // Temp where relay will cut off power
+        const u_int16_t temp_under_driveRelayCut    = NTC_NEG_20C;  // Temp where relay will cut off power
+        // charge temps
+        const u_int16_t temp_over_chargeSoftCut     = NTC_43C; // Software request charger to stop or reduce (if supported)
+        const u_int16_t temp_under_chargeSoftCut    = NTC_3C;  // Software request charger to stop or reduce (if supported)
+        const u_int16_t temp_over_chargeRelayCut    = NTC_45C; // Temp where relay will turn off
+        const u_int16_t temp_under_chargeRelayCut   = NTC_0C;  // Temp where relay will turn off
+        const u_int16_t temp_chargeTaper_1          = NTC_35C; // Temp where charger will reduce current
+        const u_int16_t temp_chargeTaper_2          = NTC_40C; // Temp where charger will reduce current more
+    };
 #endif
 
 #ifdef LG_MJ1                                       // cell parameters for LG MH1 3500mah
-    #define Vcell_HVD_Spec                  4.21f   // high voltage disconnect, charger disconnected
-    #define Vcell_Nominal_Spec              3.635f  // nominal voltage
-    #define Vcell_Low_Spec                  2.91f   // end voltage cutoff 2.5V is spec
-    #define Vcell_LVD_Spec                  2.91f   // low voltage disconnect, all off
-    #define Vcell_Balance                   4.11f   // balancing voltage
-    #define Vcell_Charge_Low_Cutoff         2.00f   // lower cutoff voltage
-    #define Vcell_Charge_High_Cutoff        4.20f   // upper cutoff voltage
-    #define Vcell_Charge_Trickle            2.80f   // voltage under where trickle charge should happen
-    #define Vcell_Charge_Bulk               4.05f   // voltage under where the bulk of the charge should happen
-    #define Vcell_Charge_Off                4.15f   // voltage above where charger should turn off
-    #define Tcell_Charge_Low_Max_Cutoff     NTC_0C  // Temp where relay will turn off
-    #define Tcell_Charge_High_Max_Cutoff    NTC_45C // Temp where relay will turn off
-    #define Tcell_Charge_Low_Cutoff         NTC_3C  // Temp where charger will not attempt to charge
-    #define Tcell_Charge_High_Cutoff        NTC_43C // Temp where charger will not attempt to charge
-    #define Tcell_Charge_Taper_1            NTC_35C // Temp where charger will reduce current
-    #define Tcell_Charge_Taper_2            NTC_40C // Temp where charger will reduce current more
+    struct celltypes
+    {
+        // voltage specifications
+        const float volt_Nominal                    = 3.64f;   // nominal voltage
+        const float volt_Balance                    = 4.11f;   // balancing voltage
+        const float volt_HVD                        = 4.21f;   // high voltage disconnect
+        const float volt_LVD                        = 2.91f;   // low voltage disconnect
+        // charge voltages
+        const float volt_high_chargeCut             = 4.20f;   // charger upper cutoff voltage
+        const float volt_low_chargeCut              = 2.00f;   // charger lower cutoff voltage
+        const float volt_chargeTrickle              = 2.80f;   // voltage under where trickle charge should happen
+        const float volt_chargeBulk                 = 4.05f;   // voltage under where the bulk of the charge should happen
+        const float volt_chargeOff                  = 4.15f;   // voltage above where charger should turn off
+        // discharge temps
+        const u_int16_t temp_over_driveRelayCut     = NTC_60C;      // Temp where relay will cut off power
+        const u_int16_t temp_under_driveRelayCut    = NTC_NEG_20C;  // Temp where relay will cut off power
+        // charge temps
+        const u_int16_t temp_over_chargeSoftCut     = NTC_43C; // Software request charger to stop or reduce (if supported)
+        const u_int16_t temp_under_chargeSoftCut    = NTC_3C;  // Software request charger to stop or reduce (if supported)
+        const u_int16_t temp_over_chargeRelayCut    = NTC_45C; // Temp where relay will turn off
+        const u_int16_t temp_under_chargeRelayCut   = NTC_0C;  // Temp where relay will turn off
+        const u_int16_t temp_chargeTaper_1          = NTC_35C; // Temp where charger will reduce current
+        const u_int16_t temp_chargeTaper_2          = NTC_40C; // Temp where charger will reduce current more
+    };
 #endif
 
   #ifdef SANYO_NCR18650B                            // cell parameters for Panasonic/Sanyo NCR18650B
-    #define Vcell_HVD_Spec                  4.30f
-    #define Vcell_Nominal_Spec              3.70f
-    #define Vcell_Low_Spec                  2.90f
-    #define Vcell_LVD_Spec                  2.90f
-    #define Vcell_Balance                   3.90f
-    #define Vcell_Charge_Low_Cutoff         2.00f
-    #define Vcell_Charge_High_Cutoff        4.20f
-    #define Vcell_Charge_Trickle            2.80f   // voltage under where trickle charge should happen  
-    #define Vcell_Charge_Bulk               4.05f   // voltage under where the bulk of the charge should happen
-    #define Vcell_Charge_Off                4.15f   // voltage above where charger should turn off
-    #define Tcell_Charge_Low_Max_Cutoff     NTC_0C; // Temp where relay will turn off
-    #define Tcell_Charge_High_Max_Cutoff    NTC_45C // Temp where relay will turn off
-    #define Tcell_Charge_Low_Cutoff         NTC_3C  // Temp where charger will not attempt to charge
-    #define Tcell_Charge_High_Cutoff        NTC_43C // Temp where charger will not attempt to charge
-    #define Tcell_Charge_Taper_1            NTC_35C // Temp where charger will reduce current
-    #define Tcell_Charge_Taper_2            NTC_40C // Temp where charger will reduce current more
+    struct celltypes
+    {
+        // voltage specifications
+        const float volt_Nominal                    = 3.70f;   // nominal voltage
+        const float volt_Balance                    = 3.90f;   // balancing voltage
+        const float volt_HVD                        = 4.30f;   // high voltage disconnect
+        const float volt_LVD                        = 2.90f;   // low voltage disconnect
+        // charge voltages
+        const float volt_high_chargeCut             = 4.20f;   // charger upper cutoff voltage
+        const float volt_low_chargeCut              = 2.00f;   // charger lower cutoff voltage
+        const float volt_chargeTrickle              = 2.80f;   // voltage under where trickle charge should happen
+        const float volt_chargeBulk                 = 4.05f;   // voltage under where the bulk of the charge should happen
+        const float volt_chargeOff                  = 4.15f;   // voltage above where charger should turn off
+        // discharge temps
+        const u_int16_t temp_over_driveRelayCut     = NTC_60C;      // Temp where relay will cut off power
+        const u_int16_t temp_under_driveRelayCut    = NTC_NEG_20C;  // Temp where relay will cut off power
+        // charge temps
+        const u_int16_t temp_over_chargeSoftCut     = NTC_43C; // Software request charger to stop or reduce (if supported)
+        const u_int16_t temp_under_chargeSoftCut    = NTC_3C;  // Software request charger to stop or reduce (if supported)
+        const u_int16_t temp_over_chargeRelayCut    = NTC_45C; // Temp where relay will turn off
+        const u_int16_t temp_under_chargeRelayCut   = NTC_0C;  // Temp where relay will turn off
+        const u_int16_t temp_chargeTaper_1          = NTC_35C; // Temp where charger will reduce current
+        const u_int16_t temp_chargeTaper_2          = NTC_40C; // Temp where charger will reduce current more
+    };
 #endif
 
 #endif
